b3b5167aff34c0dd41b4c428896e629c4cbb8f9f
Kieran <itsjusttriz@gmail.com>
2026-05-02 21:35:27 +0100

    forgot to build again

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CurseforgeService = void 0;
+var CurseforgeService_1 = require("./CurseforgeService");
+Object.defineProperty(exports, "CurseforgeService", { enumerable: true, get: function () { return CurseforgeService_1.CurseforgeService; } });
